dbc304fd2f5ca68d42b008d4b8043254e0c1b4a8
João <joao.menezes@ccc.ufcg.edu.br>
2021-05-26 17:35:16 -0300

    mudanças em processamentos de apensadas

--- process_apensadas_nao_monitoradas.R
+++ process_apensadas_nao_monitoradas.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+source(here::here("utils.R"))
 
 
 #' @title Lista proposições apensadas não monitoradas 
@@ -12,31 +13,46 @@ casa_proposicoes_apensadas_nao_monitoradas <- function(proposicoes_apensadas_fil
   proposicoes <- read_csv(proposicoes_filepath, col_types = cols(id_ext = "c"))
   proposicoes_apensadas_nao_monitoradas <- read_csv(props_apensadas_filepath, col_types = cols(id_ext = "c"))
   
-  proposicoes <- read_csv("proposicoes_camara_senado_desde_2011.csv")
+  proposicoes <- read_csv("../proposicoes-congresso-nacional/proposicoes_camara_senado_desde_2011.csv")
   
-  sigla_props_senado <- proposicoes %>%  
-    filter(!is.na(id_senado)) %>% 
-    select(id_senado, sigla_tipo_senado, numero_senado, ano_senado) %>% 
-    mutate(sigla = paste0(sigla_tipo_senado, " ", as.numeric(numero_senado), "/", ano_senado)) %>% 
-    mutate(id_ext = id_senado) %>% 
-    mutate(casa = "senado") %>% 
-    select(id_ext, casa, sigla)
-  
-  sigla_props_camara <- proposicoes %>%  
-    filter(!is.na(id_camara)) %>% 
-    select(id_camara, sigla_tipo_camara, numero_camara, ano_camara) %>% 
-    mutate(sigla = paste0(sigla_tipo_camara, " ", as.numeric(numero_camara), "/", ano_camara))%>% 
-    mutate(id_ext = id_camara) %>% 
-    mutate(casa = "camara") %>% 
-    select(id_ext, casa, sigla)
+  sigla_props <- proposicoes %>% 
+    mutate(sigla_camara = ifelse(
+      !is.na(id_camara),
+      paste0(sigla_tipo_camara, " ", as.numeric(numero_camara), "/", ano_camara),
+      NA_character_
+    )) %>%
+    mutate(sigla_senado = ifelse(
+      !is.na(id_senado),
+      paste0(sigla_tipo_senado, " ", as.numeric(numero_senado), "/", ano_senado),
+      NA_character_
+    )) %>% 
+    distinct(id_senado, id_camara, .keep_all = TRUE) %>% 
+    select(id_senado, id_camara, sigla_senado, sigla_camara)
   
   #TODO:
   # join dos dataframes de siglas camara e senado
-  props_join <- sigla_props_camara %>% 
-    full_join(sigla_props_senado, by = c("id_ext", "casa")) %>% 
-    mutate(sigla_camara = sigla.x) %>% 
-    mutate(sigla_senado = sigla.y) %>% 
-    select(id_ext, casa, sigla_camara, sigla_senado)
+  props_teste <- sigla_props %>% 
+    rowwise(.) %>%
+    mutate(
+      id_camara = ifelse(
+        !is.na(id_camara),
+        id_camara,
+        .fetch_id(
+          link_casa = NA,
+          nome = sigla,
+          casa = "camara"
+        )
+      ),
+      id_senado = ifelse(
+        !is.na(id_senado),
+        id_senado,
+        .fetch_id(
+          link_casa = NA,
+          nome = sigla,
+          casa = "senado"
+        )
+      )
+    )
   
   proposicoes_apensadas <- proposicoes_apensadas_nao_monitoradas %>%
     mutate(id_ext = as.numeric(id_ext)) %>% 
@@ -45,7 +61,20 @@ casa_proposicoes_apensadas_nao_monitoradas <- function(proposicoes_apensadas_fil
   #TODO:
   # join com csv de proposicoes apensadas para retornar nome, casa e interesses por id
   
-  props_finais <- props_join %>% 
-    inner_join(proposicoes_apensadas, by = c("id_ext", "casa"))
+  props_apensadas_camara <- proposicoes_apensadas %>% 
+    filter(casa == 'camara') %>% 
+    left_join(sigla_props, by = c("id_ext" = "id_camara")) %>% 
+    select(id_camara = id_ext, casa, interesse, id_senado, sigla_senado, sigla_camara)
+  
+  props_apensadas_senado <- proposicoes_apensadas %>% 
+    filter(casa == 'senado') %>% 
+    left_join(sigla_props, by = c("id_ext" = "id_senado")) %>% 
+    select(id_senado = id_ext, casa, interesse, id_camara, sigla_senado, sigla_camara)
+  
+  #TODO:
+  # criar função que resgata sigla pelo rcongresso a partir do id (checar ano > 2019)
+  
+  #TODO:
+  # criar função separada para criar diferentes csvs para cada interesse
  
 }
